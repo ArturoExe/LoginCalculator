3616f2643b32fe5f346aec1dcbeec8fef4ad738e
Arturo <amarturin15@gmail.com>
2026-05-21 14:16:31 -0700

    Initial Commit

---
+++ LoginCalculator/LoginCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginCalculatorApp.swift
+//  LoginCalculator
+//
+//  Created by Arturo Martinez on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct LoginCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
